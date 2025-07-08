7d160eea4ac28dc43ac827c1e078c69c516be0fe
anay-schottlabs <anay@schottlabs.com>
2025-07-07 19:23:43 -0700

    Changed baudrate to 115200

--- arduino/UFL_controller/UFL_controller.ino
+++ arduino/UFL_controller/UFL_controller.ino
@@ -2,10 +2,12 @@
 
 // Define some macros
 
-#define BAUD_RATE 9600
+#define BAUD_RATE 115200
 #define MOTOR1_FACE "U"
 #define MOTOR2_FACE "F"
 #define MOTOR3_FACE "L"
+
+// Main program methods are handled by the library
 
 void setup() {
   controllerSetup(BAUD_RATE,
