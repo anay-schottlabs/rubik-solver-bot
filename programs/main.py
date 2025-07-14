c03d634297e9b698f8c20d7ae752f20521746262
anay-schottlabs <anay@schottlabs.com>
2025-07-14 12:37:57 -0700

    Added error handling for vision.py camera errors

--- programs/main.py
+++ programs/main.py
@@ -2,7 +2,14 @@ from kociemba import solve # ruibk's cube solving module
 from vision import get_cube_state # module for computer vision
 from arduino_comm import perform_algorithm, close_connections # module for communicating with the arduinos
 
-cube_state = get_cube_state()  # Get the current state of the cube
+# have to use a try statement because the camera may not open properly
+try:
+    cube_state = get_cube_state()  # Get the current state of the cube
+except RuntimeError as e:
+    # print error and exit program
+    print(f"Error: {e}")
+    exit(1)
+
 alg_str = solve(cube_state)  # Determine algorithm to solve the cube
 
 print(f"Solution algorithm: {alg_str}") # Print the algorithm out
