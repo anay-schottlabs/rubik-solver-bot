951cc36f740dad11b7420707a217fe33b269f62a
anay-schottlabs <anay@schottlabs.com>
2025-07-14 11:58:43 -0700

    main.py logs the solution algorithm before executing

--- programs/main.py
+++ programs/main.py
@@ -3,7 +3,10 @@ from vision import get_cube_state # module for computer vision
 from arduino_comm import perform_algorithm, close_connections # module for communicating with the arduinos
 
 cube_state = get_cube_state()  # Get the current state of the cube
-alg_str = solve(cube_state)  # Solve the cube using kociemba's algorithm
+alg_str = solve(cube_state)  # Determine algorithm to solve the cube
+
+print(f"Solution algorithm: {alg_str}") # Print the algorithm out
+
 perform_algorithm(alg_str)  # Run the algorithm to solve the cube
 
 close_connections() # Close the connections to the arduinos
