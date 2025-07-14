d207dbcfe392ae948e5e7345896f2376cab6a114
anay-schottlabs <anay@schottlabs.com>
2025-07-14 11:56:14 -0700

    Finished main.py, now executes methods from vision, kociemba, and arduino_comm

--- programs/main.py
+++ programs/main.py
@@ -1,17 +1,9 @@
 from kociemba import solve # ruibk's cube solving module
 from vision import get_cube_state # module for computer vision
-from arduino_comm import perform_rotation, perform_algorithm, close_connections # module for communicating with the arduinos
+from arduino_comm import perform_algorithm, close_connections # module for communicating with the arduinos
 
-# print("INDEPENDENT MOVES:\n")
+cube_state = get_cube_state()  # Get the current state of the cube
+alg_str = solve(cube_state)  # Solve the cube using kociemba's algorithm
+perform_algorithm(alg_str)  # Run the algorithm to solve the cube
 
-# perform_rotation("U2'")
-# perform_rotation("B2'")
-# perform_rotation("L")
-# perform_rotation("R'")
-# perform_rotation("F2")
-
-# print("ALGORITHM MOVES:\n")
-
-perform_algorithm("U2 F2 L R' F2")
-
-close_connections()
+close_connections() # Close the connections to the arduinos
